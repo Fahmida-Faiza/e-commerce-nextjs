c09a8e7410ce0c348d1ed46148d3567664b1c25e
fahmida-faiza <fahmidafaiza57@gmail.com>
2024-11-19 21:55:39 +0600

    added design card

--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -3,24 +3,35 @@ import getAllBlogs from '../../../lib/getAllBlogs'
 import Link from 'next/link'
 
 export default async function Blogs() {
-const blogs = await getAllBlogs()
-// console.log(blogs)
+  const blogs = await getAllBlogs()
+  // console.log(blogs)
 
 
   return (
-    <div className='text-black'> 
+    <div className=''>
 
-    <h1>all Blogs</h1>
-    <ul className='mt-10'>
-        {blogs.map((blog )=> (
-            <li key={blog.id}>
-        <Link href={`/blogs/${blog.id}`}>
-        {blog.title}</
-        Link>
-        </li>
-        ))}
-    </ul>
+      <h1>all Blogs</h1>
 
+      <div className='grid grid-cols-3 gap-5 mx-auto w-[80%]'>
+
+
+        {
+          blogs.map((blog) => (
+            <div key={blog.id}>
+              <div className="card bg-base-100 md:w-80 lg:w-96 shadow-xl">
+                <div className="card-body">
+                  <Link href={`/blogs/${blog.id}`}>
+                    <h2 className="card-title"> {blog.title}</h2>
+                    <div className="card-actions justify-center my-10">
+                      <button className="btn btn-primary">Show Details</button>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
     </div>
   )
 }
